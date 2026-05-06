825baa93836218d14375b316feaa5534066ff1b3
ACE <135693994+acelebanski@users.noreply.github.com>
2026-05-06 20:23:27 +0200

    fix(virtual_network_gateway): Remove public IP association from ExpressRoute VNG (#204)

--- modules/virtual_network_gateway/variables.tf
+++ modules/virtual_network_gateway/variables.tf
@@ -167,24 +167,29 @@ variable "instance_settings" {
 variable "ip_configurations" {
   description = <<-EOF
   A map defining the Public IPs used by the Virtual Network Gateway.
-  
+
   Following properties are available:
   - `primary`   - (`map`, required) a map defining the primary Public IP address, following properties are available:
     - `name`                           - (`string`, required) name of the IP config.
     - `create_public_ip`               - (`bool`, optional, defaults to `true`) controls if a Public IP is created or sourced.
+                                         Must be set to `false` for `ExpressRoute` type gateways.
     - `public_ip_name`                 - (`string`, optional) name of a Public IP resource, required unless `public_ip` module
                                          and `public_ip_id` property are used. Depending on the value of `create_public_ip`
                                          property, this will be a name of a newly created or existing resource (for values of
-                                         `true` and `false` accordingly).
+                                         `true` and `false` accordingly). Must be `null` for `ExpressRoute` type gateways.
     - `public_ip_resource_group_name`  - (`string`, optional, defaults to the Load Balancer's RG) name of a Resource Group
-                                         hosting an existing Public IP resource.
+                                         hosting an existing Public IP resource. Must be `null` for `ExpressRoute` type gateways.
     - `public_ip_id`                   - (`string`, optional, defaults to `null`) ID of the public IP to associate with the
                                          interface. Property is used when public IP is not created or sourced within this module.
+                                         Must be `null` for `ExpressRoute` type gateways.
     - `dynamic_private_ip_allocation`  - (`bool`, optional, defaults to `true`) controls if the private IP address is assigned
                                          dynamically or statically.
   - `secondary` - (`map`, optional, defaults to `null`) a map defining the secondary Public IP address resource. Required only
                   for `type` set to `Vpn` and `active-active` set to `true`. Same properties available as for `primary` property.
 
+  **Note!** \
+  For `instance_settings.type` set to `ExpressRoute`, Public IPs are created automatically and fully managed by Azure platform. 
+  In that case, `create_public_ip` must be set to `false`, and both `public_ip_name` and `public_ip_id` must be `null`.
   EOF
   type = object({
     primary = object({
@@ -209,19 +214,6 @@ variable "ip_configurations" {
     ) : true
     error_message = <<-EOF
     The `name` property has to be unique among all IP configurations.
-    EOF
-  }
-  validation { # public_ip_id, public_ip_name
-    condition = alltrue([
-      (var.ip_configurations.primary.public_ip_name != null || var.ip_configurations.primary.public_ip_id != null),
-      (
-        var.ip_configurations.secondary != null ? (
-          var.ip_configurations.secondary.public_ip_name != null || var.ip_configurations.secondary.public_ip_id != null
-        ) : true
-      )
-    ])
-    error_message = <<-EOF
-    Either `public_ip_name` or `public_ip_id` property must be set.
     EOF
   }
   validation { # public_ip_id, create_public_ip, public_ip_name
